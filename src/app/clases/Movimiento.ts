f328448e65de08dd2a7313be15055b9d9bef2a89
Fernando Carta <cartafernando@outlook.com>
2025-05-15 16:57:53 -0500

    Clases que representan a Producto y movimiento

---
+++ src/app/clases/Movimiento.ts
@@ -0,0 +1,8 @@
+export class Movimiento {
+  private id: number;
+  private idProducto: number;
+  private tipo: string;
+  private cantidad: number;
+  private fecha: Date;
+  private descripcion: string;
+}
---
+++ src/app/clases/Producto.ts
@@ -0,0 +1,10 @@
+export class Producto {
+  private id: number;
+  private nombre: string;
+  private descripcion: string;
+  private precio: number;
+  private stock: number;
+  private categoria: string;
+  private codigo: string;
+  private fechaCreacion: Date;
+}
